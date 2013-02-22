ef6c59899528969bff65c5d5dd0e80d88fbadbb3
Andrew McGrath <amcgrath@angel.com>
2013-02-22 09:50:41 -0500

    implementation of interface

--- src/main/java/com/drewmcgrath/datemagic/notification/SmsMatchNotifier.java
+++ src/main/java/com/drewmcgrath/datemagic/notification/SmsMatchNotifier.java
@@ -4,9 +4,17 @@
  */
 package com.drewmcgrath.datemagic.notification;
 
+import com.drewmcgrath.datemagic.domain.Profile;
+import java.util.Collection;
+
 /**
  *
  * @author amcgrath
  */
 public class SmsMatchNotifier implements MatchNotifier {
+
+    @Override
+    public void notify(Profile profile, Collection<Profile> matches) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
