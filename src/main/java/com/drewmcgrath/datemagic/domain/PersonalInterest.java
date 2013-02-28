145db31ab4ed2d559995abdad752eff497fdd970
Andrew McGrath <amcgrath@angel.com>
2013-02-28 12:15:36 -0500

    enum unit test with null and equals

--- src/main/java/com/drewmcgrath/datemagic/domain/PersonalInterest.java
+++ src/main/java/com/drewmcgrath/datemagic/domain/PersonalInterest.java
@@ -1,5 +1,5 @@
-/*
- * Copyright Angel.com 2011
+/**
+ * Copyright drewmcgrath.com 2011
  *
  */
 package com.drewmcgrath.datemagic.domain;
@@ -10,4 +10,47 @@ package com.drewmcgrath.datemagic.domain;
  */
 public enum PersonalInterest {
 
+    DINNING("Dinning"),
+    DANCING("Dancing"),
+    UNDER_WATER_BASKET_WEAVING("Underwater Basket Weaving");
+    /**
+     * The string that will be displayed on the webpage
+     */
+    private String displayText;
+
+    /**
+     * Constructor
+     *
+     * @param displayText
+     */
+    private PersonalInterest(String displayText) {
+        this.displayText = displayText;
+    }
+
+    /**
+     * Returns the display text for the enum
+     *
+     * @return
+     */
+    public String getDisplayText() {
+        return displayText;
+    }
+
+    /**
+     * Returns an enum for the string passed assuming it matches the display
+     * text.
+     *
+     * @param text
+     * @return the enum that matches the display text or null if nothing matches
+     */
+    public static PersonalInterest fromDisplayText(String text) {
+        if (text != null) {
+            for (PersonalInterest interest : PersonalInterest.values()) {
+                if (interest.getDisplayText().equalsIgnoreCase(text)) {
+                    return interest;
+                }
+            }
+        }
+        return null;
+    }
 }
